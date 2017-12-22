6889a6b2dd735f8e5e9bd0c73cc2d23006013304
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2017-12-22 00:02:07 -0200

    src: refactor bin file

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,8 @@ var path = require('path');
 var fs = require('fs');
 var { rootPath } = require('get-root-path');
 var automated = require('./../dist/index.js');
+var browserstack = require('browserstack-local');
+var ora = require('ora');
 
 function loadJSON (filePath) {
   var data = fs.readFileSync(filePath);
@@ -13,17 +15,53 @@ function loadJSON (filePath) {
 function hasLocal (args) {
   const local = args.find(function (argument) {
     return argument === '--local';
-  })
+  });
 
-  return local ? true : false
+  return local ? true : false;
 }
 
+var finishedTestsCount = 0
+var browserstackLocal = new browserstack.Local()
+var spinner = ora('Loading tests').start()
 var configsPath = path.join(rootPath, './.browserstack');
 var config = loadJSON(configsPath);
 var args = process.argv.slice(2);
-var isLocal = hasLocal(args);
+config.isLocal = hasLocal(args);
+var countTests = config.browsers.length
+spinner.text = getEndTestMessage(finishedTestsCount);
 
-automated(
-  config,
-  isLocal,
-)
+function getEndTestMessage(finishedTests) {
+  return 'Completed '
+    + finishedTests
+    + ' of '
+    + countTests
+    + ' with concurrency '
+    + config.concurrency;
+}
+
+function endTest () {
+  spinner.text = getEndTestMessage(++finishedTestsCount);
+}
+
+function endAllTests () {
+  if (browserstackLocal.isRunning()) {
+    browserstackLocal.stop()
+  }
+  spinner.stop()
+}
+
+config.endTestCallback = endTest
+config.endAllTestsCallback = endAllTests
+
+if (config.isLocal) {
+  spinner.text = 'Up Browserstack local binary';
+  browserstackLocal.start({
+    key: config.remote.pwd,
+  }, function () {
+    spinner.text = getEndTestMessage(finishedTestsCount);
+    automated(config);
+  })
+} else {
+  spinner.text = getEndTestMessage(finishedTestsCount);
+  automated(config);
+}
