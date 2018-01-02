575b4d5295c32caf79e1bea261ad96d88f667309
Vitor Capretz <capretzvitor@gmail.com>
2018-01-02 14:15:18 -0200

    chore: fix example running
    it removes the local checking as it is going to be made in #8
    fix path to examples files

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const { join } = require('path')
 const { readFileSync } = require('fs')
 const { rootPath } = require('get-root-path')
 const {
-  isEmpty,
   length,
-  add,
+  inc,
 } = require('ramda')
-const automated = require('./src/index.js')
-const browserstack = require('browserstack-local')
+const { prepare: automated } = require('./src/index.js')
+
 const ora = require('ora')
 
 const loadJSON = (filePath) => {
@@ -17,55 +16,29 @@ const loadJSON = (filePath) => {
   return JSON.parse(file)
 }
 
-const hasLocal = (args) => {
-  const local = args.find(argument => (argument === '--local'))
-
-  return isEmpty(local)
-}
-
-const plus1 = add(1)
-
 let finishedTestsCount = 0
-const browserstackLocal = new browserstack.Local()
+
 const spinner = ora('Loading tests').start()
-const configsPath = join(rootPath, './.browserstack')
+const configsPath = join(rootPath, './examples/.robrowser')
 const config = loadJSON(configsPath)
 const { concurrency, browsers } = config
-const args = process.argv.slice(2)
-config.isLocal = hasLocal(args)
+
 const countTests = length(browsers)
 const getEndTestMessage = finishedTests =>
   `Completed ${finishedTests} of ${countTests} with currency ${concurrency}`
 spinner.text = getEndTestMessage(finishedTestsCount)
 
 const endTest = () => {
-  finishedTestsCount = plus1(finishedTestsCount)
+  finishedTestsCount = inc(finishedTestsCount)
   spinner.text = getEndTestMessage(finishedTestsCount)
 }
 
 const endAllTests = () => {
-  if (browserstackLocal.isRunning()) {
-    spinner.text = 'Stop browserstack local binary'
-    browserstackLocal.stop(() => {
-      spinner.stop()
-    })
-  } else {
-    spinner.stop()
-  }
+  spinner.stop()
 }
 
 config.endTestCallback = endTest
 config.endAllTestsCallback = endAllTests
 
-if (config.isLocal) {
-  spinner.text = 'Up Browserstack local binary'
-  browserstackLocal.start({
-    key: config.remote.pwd,
-  }, () => {
-    spinner.text = getEndTestMessage(finishedTestsCount)
-    automated(config)
-  })
-} else {
-  spinner.text = getEndTestMessage(finishedTestsCount)
-  automated(config)
-}
+spinner.text = getEndTestMessage(finishedTestsCount)
+automated(config)
