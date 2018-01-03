8579fe4b4a0c619d8eb8ef98cfa4904dd31ab455
Augusto Amaral <augusto.jm.amaral1@gmail.com>
2018-01-03 18:33:54 -0200

    example: fix find element
    Add waitFor method to wait show element in the
    browser.

--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,7 @@ module.exports = (browser, next) => {
     .waitForElementByName('q', 5000)
     .elementByName('q')
     .sendKeys('BrowserStack')
+    .waitForElementByClassName('lsb', 5000)
     .elementByClassName('lsb')
     .click()
     .saveScreenshot('google_search.png')
